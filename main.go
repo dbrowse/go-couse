9e99c9f7db69de47e5fda8aed28ed96487aebc09
Georgiev <dige@softwareag.com>
2022-07-18 11:23:44 +0300

    Initial commit

--- main.go
+++ main.go
@@ -1,18 +1,12 @@
 package main
 
 import (
-	"fmt"
+	
 	"net/http"
 )
 
 const portNumber = ":8080"
 
-func Home (w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "This is the home page")
-}
-func About (w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "This is the about page")
-}
 
 func  main ()  {
 
